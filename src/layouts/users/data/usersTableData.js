6f27a3ad211c67728cc5e68081cca89596964374
Eric Li <jiliangli0904@gmail.com>
2022-11-16 17:39:17 -0600

    Eric Nov 16

--- src/layouts/users/data/usersTableData.js
+++ src/layouts/users/data/usersTableData.js
@@ -20,82 +20,77 @@ Coded by www.creative-tim.com
 import MDTypography from "components/MDTypography";
 // import MDBadge from "components/MDBadge";
 
-export default function data() {
+// Eric; populate and return each user entry
+function populateEntry(fname, minit, lname, dob, phoneNum, plate, type, lid, spaceNo, checkInDate) {
+  return {
+    name: (
+      <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+        {[fname, " ", minit, " ", lname]}
+      </MDTypography>
+    ),
+    dob: (
+      <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+        {dob}
+      </MDTypography>
+    ),
+    phone: (
+      <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+        {phoneNum === null ? "N/A" : phoneNum}
+      </MDTypography>
+    ),
+    plate: (
+      <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+        {plate}
+      </MDTypography>
+    ),
+    type: (
+      <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+        {type}
+      </MDTypography>
+    ),
+    lot: (
+      <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+        {lid === null ? "N/A" : lid}
+      </MDTypography>
+    ),
+    space: (
+      <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+        {spaceNo === null ? "N/A" : spaceNo}
+      </MDTypography>
+    ),
+    time: (
+      <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+        {checkInDate === null ? "N/A" : checkInDate}
+      </MDTypography>
+    ),
+  };
+}
+
+// Eric; returns a new list for rows given new backend response
+const getNewList = (resp) => {
+  const result = [];
+  resp.forEach((ele) => {
+    result.push(
+      populateEntry(ele[0], ele[1], ele[2], ele[3], ele[4], ele[5], ele[6], ele[7], ele[8], ele[9])
+    );
+  });
+
+  return result;
+};
+
+export default function data(resp) {
   return {
     columns: [
       { Header: "Name", accessor: "name", align: "left" },
-      { Header: "Phone Number", accessor: "phone", align: "center" },
       { Header: "Date of Birth", accessor: "dob", align: "center" },
+      { Header: "Phone Number", accessor: "phone", align: "center" },
       { Header: "Vehicle Plate Number", accessor: "plate", align: "center" },
+      { Header: "Vehicle Type", accessor: "type", align: "center" },
       { Header: "Parking Lot", accessor: "lot", align: "center" },
       { Header: "Parking Space", accessor: "space", align: "center" },
+      { Header: "Check-In Time", accessor: "time", align: "center" },
     ],
 
-    rows: [
-      {
-        name: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Peter Parker
-          </MDTypography>
-        ),
-        phone: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            123-123-1321
-          </MDTypography>
-        ),
-        dob: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Jun 2 2022
-          </MDTypography>
-        ),
-        plate: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            BCC1765
-          </MDTypography>
-        ),
-        lot: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            A12
-          </MDTypography>
-        ),
-        space: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            1234
-          </MDTypography>
-        ),
-      },
-      {
-        name: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Parker Peter
-          </MDTypography>
-        ),
-        phone: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            325-965-8898
-          </MDTypography>
-        ),
-        dob: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Feb 30 2000
-          </MDTypography>
-        ),
-        plate: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            NULL
-          </MDTypography>
-        ),
-        lot: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            NULL
-          </MDTypography>
-        ),
-        space: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            NULL
-          </MDTypography>
-        ),
-      },
-    ],
+    rows: getNewList(resp),
   };
 }
